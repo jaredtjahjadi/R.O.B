bd2794738e6bb0607aa2ecdfa059d2687140ea2d
JareBear16 <jaredtjahjadi@gmail.com>
2021-02-21 00:19:07 -0500

    Haha coin flip command go brrt

---
+++ commands/fun/coinflip.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "coinflip",
+    aliases: ["flip"],
+    description: "Flips a (virtual) coin.",
+    args: false,
+    usage: "",
+    guildOnly: false,
+    execute(message, args) {
+        const r = Math.floor(Math.random() * 2 + 1);
+        message.channel.send("Flipping coin...").then(msg => {
+            setTimeout(
+                function headsOrTails() {
+                    if(r == 1) msg.edit("Heads!");
+                    else msg.edit("Tails!");
+                }, 2000);
+        });
+    }
+}
